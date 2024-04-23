314fd7598a596af063d64025bfa1e0114de0834f
kaja <kaja.sund@googlemail.com>
2024-04-23 12:51:27 +0200

    Migrate project to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
+import {NavigationComponent} from "./navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'buddyup';
+  title = 'auto-kino';
 }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';  //Importiere die Component-Dekorator
 //Hier teilt diese der HeaderComponent Klasse mit, dass sie eine Angular-Komponente ist und gibt die Meta-Daten an
 @Component({
   standalone: true,
-  selector: "app-header",                 //Referenz auf die HTML-Datei
-  templateUrl: "header.component.html",   //Aussehen der Komponente wird hier definiert
-  styleUrls: ['header.component.css']     //Style-Datei, die auf diese Komponente angewendet werden soll
+  selector: "app-navigation",                 //Referenz auf die HTML-Datei
+  templateUrl: "navigation.component.html",   //Aussehen der Komponente wird hier definiert
+  styleUrls: ['navigation.component.css']     //Style-Datei, die auf diese Komponente angewendet werden soll
 })
 
 //Quasi die main class der Komponente
-export class HeaderComponent {
+export class NavigationComponent {
 
 }
