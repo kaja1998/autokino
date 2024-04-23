c3fbeaef3cf16f5985f147f25d98a935c6bae358
kaja <kaja.sund@googlemail.com>
2024-04-23 11:56:53 +0200

    Heade Komponente works!

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';  //Importiere die Component-Dekorator
 //Dekoriere bzw. markiert die Header-Klasse mit der Component-Dekorator-Funktion
 //Hier teilt diese der HeaderComponent Klasse mit, dass sie eine Angular-Komponente ist und gibt die Meta-Daten an
 @Component({
+  standalone: true,
   selector: "app-header",                 //Referenz auf die HTML-Datei
   templateUrl: "header.component.html",   //Aussehen der Komponente wird hier definiert
   styleUrls: ['header.component.css']     //Style-Datei, die auf diese Komponente angewendet werden soll
