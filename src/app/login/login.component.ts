d637df783d921abec03e72c45423fc4074894c43
Kaja <111892274+kaja1998@users.noreply.github.com>
2024-05-06 15:52:02 +0200

    Anmelden und registrieren fertig

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  showLoginForm: boolean = true; // Gibt an, ob das Anmeldeformular sichtbar ist
+  showRegisterForm: boolean = false; // Gibt an, ob das Registrierungsformular sichtbar ist
+  showLoginButton: boolean = true;
+  showRegisterButton: boolean = false;
+
+  toggleForms(): void {
+    this.showLoginForm = !this.showLoginForm;
+    this.showRegisterForm = !this.showRegisterForm;
+    this.showLoginButton = !this.showLoginButton;
+    this.showRegisterButton = !this.showRegisterButton;
+  }
+
 }
