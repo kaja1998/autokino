01345fd08a06ef8b9e0f82806c0915d165d9525f
Fux <104574967+FuxDerFuchs@users.noreply.github.com>
2024-05-06 17:18:12 +0200

    programm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './home/start.component';
+import { ProgrammComponent } from './programm/programm.component';
 
 export const routes: Routes = [
     {path: '', component: StartComponent,},
     {path: 'home', component: StartComponent,},
+    {path: 'programm', component: ProgrammComponent},
 ];
---
+++ src/app/programm/programm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programm',
+  standalone: true,
+  imports: [],
+  templateUrl: './programm.component.html',
+  styleUrl: './programm.component.css'
+})
+export class ProgrammComponent {
+
+}
