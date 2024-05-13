61921244aaa7219ad9871270dde6420a7e5f9ab1
Fux <104574967+FuxDerFuchs@users.noreply.github.com>
2024-05-13 13:15:49 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import {Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import { FilmdetailsComponent } from './filmdetails/filmdetails.component';
 import { StartComponent } from './home/start.component';
+import { ProgrammComponent } from "./programm/programm.component";
 
 export const routes: Routes = [
     {path: '', component: StartComponent,},
