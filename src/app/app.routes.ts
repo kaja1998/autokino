30055102baa009e364f280791c443abafda2d9d3
Kaja <111892274+kaja1998@users.noreply.github.com>
2024-05-13 17:44:53 +0200

    Routing, Kleinigkeiten und Bild

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { StartComponent } from './home/start.component';
 import {LoginComponent} from "./login/login.component";
 import { FilmdetailsComponent } from './filmdetails/filmdetails.component';
+import {KundenkontoComponent} from "./kundenkonto/kundenkonto.component";
+import {KundenkontoBearbeitenComponent} from "./kundenkonto-bearbeiten/kundenkonto-bearbeiten.component";
 
 export const routes: Routes = [
     {path: '', component: StartComponent,},
     {path: 'home', component: StartComponent},
     {path: 'filmdetails', component: FilmdetailsComponent},
     {path: 'login', component: LoginComponent,},
+    {path: 'kundenkonto', component: KundenkontoComponent,},
+  {path: 'kundenkonto/bearbeiten', component: KundenkontoBearbeitenComponent,},
 ];
