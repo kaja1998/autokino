46725ed2d7ea2b8e56acffce19d91845db6a292e
Fux <104574967+FuxDerFuchs@users.noreply.github.com>
2024-05-13 12:41:41 +0200

    filme fertisch

--- src/app/programm/programm.component.ts
+++ src/app/programm/programm.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-programm',
@@ -7,7 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './programm.component.html',
   styleUrl: './programm.component.css'
 })
-export class ProgrammComponent {
 
+export class ProgrammComponent implements OnInit {
+  
+  ngOnInit(): void {
+    // Hier füge den JavaScript-Code ein
+
+    const clearIcon = document.querySelector(".clear-icon") as HTMLElement;
+    const searchBar = document.querySelector(".search") as HTMLInputElement;
+
+    searchBar.addEventListener("keyup", () => {
+      if (searchBar.value && clearIcon.style.visibility != "visible") {
+        clearIcon.style.visibility = "visible";
+      } else if (!searchBar.value) {
+        clearIcon.style.visibility = "hidden";
+      }
+    });
+
+    clearIcon.addEventListener("click", () => {
+      searchBar.value = "";
+      clearIcon.style.visibility = "hidden";
+    });
+  }
 
 }
