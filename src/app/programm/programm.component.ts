c47e32b0e0a8ff082857adc173f5f0f43da1d9ed
FuxUwU <104574967+FuxDerFuchs@users.noreply.github.com>
2024-05-30 15:56:49 +0200

    Zum Film Button

--- src/app/programm/programm.component.ts
+++ src/app/programm/programm.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-programm',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './programm.component.html',
   styleUrl: './programm.component.css'
 })
@@ -11,7 +12,6 @@ import { Component, OnInit } from '@angular/core';
 export class ProgrammComponent implements OnInit {
   
   ngOnInit(): void {
-    // Hier füge den JavaScript-Code ein
 
     const clearIcon = document.querySelector(".clear-icon") as HTMLElement;
     const searchBar = document.querySelector(".search") as HTMLInputElement;
