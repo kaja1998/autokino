e26c9f7fc86fc97f6b58e679d7571d0bf732f85e
LasseNeu21 <neumann-lasse@web.de>
2024-06-10 18:00:15 +0200

    Server Lasse

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var con = mysql.createConnection({
       host: "127.0.0.1",
       port: "3306",
       user: "root",
-      password: "Password1!"
+      password: "Egal!234"
 });
 
 app.get('/filme', function (req, res) {
