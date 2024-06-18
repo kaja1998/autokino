cf82384dea25208a3c1edb9ed2219474e9215534
Greta Sund <greta.sund@gmail.com>
2024-06-18 18:08:02 +0800

    Save data of authenticated user in localStorage and display the user's first name on the home page

--- src/app/kundenkonto/kundenkonto.component.ts
+++ src/app/kundenkonto/kundenkonto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {LoginAuthenticationService} from "../providers/login-authentication.service";
 
@@ -9,8 +9,12 @@ import {LoginAuthenticationService} from "../providers/login-authentication.serv
   templateUrl: './kundenkonto.component.html',
   styleUrl: './kundenkonto.component.css'
 })
-export class KundenkontoComponent {
+export class KundenkontoComponent implements OnInit {
 
-  vorname: string = "";
+  user: any = "";
+
+  ngOnInit(): void {
+    this.user = localStorage.getItem('user');
+  }
 
 }
