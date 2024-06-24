f7d0844eaf7ccf4e72a4c000262a0dac98366b4f
Fux <104574967+FuxDerFuchs@users.noreply.github.com>
2024-06-24 09:26:10 +0200

    Beispiel Link von Programm zu Filmdetails

--- src/app/filmdetails/filmdetails.component.ts
+++ src/app/filmdetails/filmdetails.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-filmdetails',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './filmdetails.component.html',
   styleUrl: './filmdetails.component.css'
 })
+
 export class FilmdetailsComponent {
+  constructor(private route: ActivatedRoute) { }
+
+  filmtitel: string = ""
+
+  ngOnInit(): void {
+    this.filmtitel = this.route.snapshot.paramMap.get('filmtitel') ?? '';
+  }
+
 }
